e1f41810c4761091b3e7fd884535263af6678308
Majid-pkz <majid.computing@gmail.com>
2023-05-04 16:42:55 +1000

    add controller

---
+++ server/data/products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: 'Bose QuietComfort 35 Wireless Headphones',
+    image: '/images/headphones1.jpg',
+    description:
+      'Industry-leading noise cancellation makes quiet sound quieter and music sound better. Wireless, with easy Bluetooth and NFC pairing, and up to 20 hours of battery life.',
+    brand: 'Bose',
+    category: 'Electronics',
+    price: 249.99,
+    countInStock: 8,
+    rating: 4.7,
+    numReviews: 20,
+  },
+  {
+    name: 'Samsung Galaxy S21 Ultra 5G',
+    image: '/images/phone1.jpg',
+    description:
+      'Experience Samsung Galaxy 21 Ultra 5G with an all-new camera featuring 108MP on a 6.8" display, 8K video, and a 5000mAh battery. It is designed to be epic in every way.',
+    brand: 'Samsung',
+    category: 'Electronics',
+    price: 899.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 15,
+  },
+  {
+    name: 'Nikon Z7 II Mirrorless Camera',
+    image: '/images/camera1.jpg',
+    description:
+      'The Nikon Z7 II Mirrorless Camera features a 45.7MP sensor and 4K UHD video capabilities, with advanced autofocus and image processing for stunning results.',
+    brand: 'Nikon',
+    category: 'Electronics',
+    price: 2899.99,
+    countInStock: 3,
+    rating: 4.8,
+    numReviews: 10,
+  },
+  {
+    name: 'Razer BlackWidow V3 Pro Wireless Gaming Keyboard',
+    image: '/images/keyboard1.jpg',
+    description:
+      'Experience faster, more responsive gaming with the Razer BlackWidow V3 Pro wireless gaming keyboard, featuring Razer HyperSpeed technology and customizable RGB lighting.',
+    brand: 'Razer',
+    category: 'Electronics',
+    price: 219.99,
+    countInStock: 6,
+    rating: 4.3,
+    numReviews: 18,
+  },
+  {
+    name: 'Google Nest Hub (2nd Gen)',
+    image: '/images/hub1.jpg',
+    description:
+      'Get help managing your smart home, streaming media, and keeping track of your schedule with the Google Nest Hub (2nd Gen). Featuring a 7-inch touchscreen display and voice control with Google Assistant.',
+    brand: 'Google',
+    category: 'Smart Home',
+    price: 99.99,
+    countInStock: 12,
+    rating: 4.6,
+    numReviews: 25,
+  },
+];
+
+export default products;
---
+++ server/data/user.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin',
+    email: 'admin@a.com',
+    password: bcrypt.hashSync('Pass123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Majid Pk',
+    email: 'majid@pk.com',
+    password: bcrypt.hashSync('Pass123456', 10),
+  },
+  {
+    name: 'Emma Jones',
+    email: 'emma@j.com',
+    password: bcrypt.hashSync('Pass123456', 10),
+  },
+];
+
+export default users;
