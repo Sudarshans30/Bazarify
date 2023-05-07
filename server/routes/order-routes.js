476deb209ec6300fa62cb786fe5840de0748b2ed
Majid-pkz <majid.computing@gmail.com>
2023-05-07 17:26:43 +1000

    modify order route based on auth

--- server/routes/order-routes.js
+++ server/routes/order-routes.js
@@ -8,12 +8,13 @@ import {
   updateOrderToDelivered,
   getOrders,
 } from '../controller/order-controller.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
+import { authentication, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
-router.route('/mine').get(protect, getMyOrders);
-router.route('/:id').get(protect, getOrderById);
-router.route('/:id/pay').put(protect, updateOrderToPaid);
-router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+router.route('/').post(authentication, addOrderItems).get(authentication, admin, getOrders);
+router.route('/mine').get(authentication, getMyOrders);
+router.route('/:id').get(authentication, getOrderById);
+router.route('/:id/pay').put(authentication, updateOrderToPaid);
+router.route('/:id/deliver').put(authentication, admin, updateOrderToDelivered);
 
 export default router;
+
