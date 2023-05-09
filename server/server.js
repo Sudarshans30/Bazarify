b31e2dc18db98f81c5e56dd77ab2a9fbdea405e9
Sudarshan singh <sudarshans471@gmail.com>
2023-05-09 13:56:17 +1000

    addedd

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/connection.js';
-import productRoutes from './routes/product-Routes.js';
-import userRoutes from './routes/user-Routes.js';
+import productRoutes from './routes/product-routes.js';
+import userRoutes from './routes/user-routes.js';
 import orderRoutes from './routes/order-routes.js';
-import imageRoutes from './routes/image-Routes.js';
+import imageRoutes from './routes/image-routes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5002;
@@ -38,12 +38,11 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 }
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....');
-//   });
-// }
 
 app.use(notFound);
 app.use(errorHandler);
